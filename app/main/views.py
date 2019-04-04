11f2b16b6f200bf1136bc2f38e769d2f0082d13f
jingw222 <jingw222@gmail.com>
2019-04-05 00:06:39 +0800

    Fix a session bug

--- app/main/views.py
+++ app/main/views.py
@@ -11,19 +11,18 @@ from .gpt2.gpt2_model import interact_model
 def index():
     form = TextForm()
     if form.validate_on_submit():
-        old_text = session.get('text')
+        old_text = session.get('text', None)
         if old_text is not None and old_text != form.text.data:
             flash('New input submitted!')
-
-        session['text'] = form.text.data
-        session['output'] = interact_model(form.text.data)
+            session['text'] = form.text.data
+            session['output'] = interact_model(form.text.data)
 
         line = Line.query.filter_by(text=form.text.data).first()
         if line is None:
             line = Line(
                 type='custom',
-                text=form.text.data,
-                output=session['output'])
+                text=session.get('text', None),
+                output=session.get('output', None))
             db.session.add(line)
             db.session.commit()
             session['known'] = False
